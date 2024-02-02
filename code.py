ef1903c4eb4b2b97baaebfeb36be3152c0ce84a3
Scott Barrett <45408775+sabarrett328@users.noreply.github.com>
2024-02-02 09:51:59 -0500

    Added comments and cleaned up some other items

--- code.py
+++ code.py
@@ -1,4 +1,30 @@
+# *************************************************************************
+# *************************************************************************
+# 
+# SBMagtag Weather Station
+# My version of the Adafruit Magtag weather program
+# 
+# I edited many items:
+# 	My style is to only use functions when I see a way to reduce code length
+#		All the routines here were just unnecessary in my opinion
+#	I removed all the extra code for conditions that I wouldn't need
+#	    when this runs in my house
+#	I found some errors and corrected them
+#	I changed to the onecall 3.0 api, and subscribed.  the first 1000 calls
+#		are free, the cost is workable, and it accomodated the change of units
+#	I added many data points and updated the screen background to accomodate the 
+#		extra data
+#	I added the aqi api data
+#		
+#
+# *************************************************************************
+# *************************************************************************
 
+# ----------------------------
+# Preliminary items to setup 
+# ----------------------------
+
+# --| Imports |--------------------------
 import time
 import terminalio
 import displayio
@@ -15,11 +41,7 @@ key = secrets["openweather_token"]
 weather_url = "https://api.openweathermap.org/data/3.0/onecall?units=imperial&exclude=minutely,hourly,alerts&"
 aqi_url = "https://api.openweathermap.org/data/2.5/air_pollution?"
 
-# -------------------------------------------
-
-# ----------------------------
-# Define various assets
-# ----------------------------
+# --| Define program assets |--------------------------
 BACKGROUND_BMP = "/bmps/weather_bg.bmp"
 ICONS_LARGE_FILE = "/bmps/weather_icons_70px.bmp"
 ICONS_SMALL_FILE = "/bmps/weather_icons_20px.bmp"
@@ -38,22 +60,23 @@ MONTHS = (
     "October",
     "November",
     "December",
-)
+	)
+# --| Define the board |--------------------------
 magtag = MagTag()
 
-# ----------------------------
-# Backgrounnd bitmap
-# ----------------------------
+# --| Background bitmap |--------------------------
 magtag.graphics.set_background(BACKGROUND_BMP)
 
-# ----------------------------
-# Weather icons sprite sheet
-# ----------------------------
+# --| Sprint Sheets |--------------------------
 icons_large_bmp, icons_large_pal = adafruit_imageload.load(ICONS_LARGE_FILE)
 icons_small_bmp, icons_small_pal = adafruit_imageload.load(ICONS_SMALL_FILE)
 
 # /////////////////////////////////////////////////////////////////////////
 
+
+# =========================================================================
+# Functions for implementation
+# =========================================================================
 def make_banner(x=0, y=0):
     """Make a single future forecast info banner group."""
     day_of_week = label.Label(terminalio.FONT, text="DAY", color=0x000000)
@@ -69,7 +92,7 @@ def make_banner(x=0, y=0):
         height=1,
         tile_width=20,
         tile_height=20,
-    )
+		)
 
     day_temp = label.Label(terminalio.FONT, text="+100F", color=0x000000)
     day_temp.anchor_point = (0, 0.5)
@@ -88,34 +111,56 @@ def update_banner(banner, data):
     banner[1][0] = ICON_MAP.index(data["weather"][0]["icon"][:2])
     banner[2].text = "{:3.0f}".format(data["temp"]["min"]) + "/" + "{:3.0f}".format(data["temp"]["max"])
 
+# =========================================================================
+# Define the user HMI elements for the screen
+# =========================================================================
+curdt = label.Label(terminalio.FONT, text="date", color=0x000000)
+curdt.anchor_point = (0, 0)
+curdt.anchored_position = (15, 14)
 
-
-def go_to_sleep(current_time):
-    hour, minutes, seconds = time.localtime(current_time)[3:6]
-    # if its after 10pm, sleep till 5 am.  if not, sleep for an hour 
-    if hour >21:
-        seconds_to_sleep = (((24-hour)*3600)-(minutes*60) + (3600*5))
-    else:
-        seconds_to_sleep = 3600
-
-    print(
-        "Sleeping for {} hours, {} minutes".format(
-            seconds_to_sleep // 3600, (seconds_to_sleep // 60) % 60
-        )
-    )
-    magtag.exit_and_deep_sleep(seconds_to_sleep)
-
-
-# ===========
-# U I
-# ===========
-curtim = label.Label(terminalio.FONT, text="?" * 30, color=0x000000)
+curtim = label.Label(terminalio.FONT, text="time" * 30, color=0x000000)
 curtim.anchor_point = (0, 0)
 curtim.anchored_position = (15, 24)
 
-curdt = label.Label(terminalio.FONT, text="time", color=0x000000)
-curdt.anchor_point = (0, 0)
-curdt.anchored_position = (15, 14)
+curtemp = label.Label(terminalio.FONT, text="+100F", color=0x000000)
+curtemp.anchor_point = (0, 0)
+curtemp.anchored_position = (150, 21)
+
+#curfeels = label.Label(terminalio.FONT, text="+100F", color=0x000000)
+#curfeels.anchor_point = (0, 0)
+#curfeels.anchored_position = (150, 21)
+
+curprs = label.Label(terminalio.FONT, text="99.99", color=0x000000)
+curprs.anchor_point = (0, 0)
+curprs.anchored_position = (105, 94)
+
+curwind = label.Label(terminalio.FONT, text="99m/s", color=0x000000)
+curwind.anchor_point = (0, 0)
+curwind.anchored_position = (150, 70)
+
+#curgust = label.Label(terminalio.FONT, text="99m/s", color=0x000000)
+#curgust.anchor_point = (0, 0)
+#curgust.anchored_position = (150, 70)
+
+curhumid = label.Label(terminalio.FONT, text="100%", color=0x000000)
+curhumid.anchor_point = (0, 0)
+curhumid.anchored_position = (118, 70)
+
+curuv = label.Label(terminalio.FONT, text="100%", color=0x000000)
+curuv.anchor_point = (0, 0)
+curuv.anchored_position = (160, 94)
+
+#curaq = label.Label(terminalio.FONT, text="1", color=0x000000)
+#curaq.anchor_point = (0, 0)
+#curaq.anchored_position = (160, 94)
+
+curcloud = label.Label(terminalio.FONT, text="100", color=0x000000)
+curcloud.anchor_point = (0, 0)
+curcloud.anchored_position = (165, 50)
+
+#curvis = label.Label(terminalio.FONT, text="100", color=0x000000)
+#curvis.anchor_point = (0, 0)
+#curvis.anchored_position = (165, 50)
 
 curicon = displayio.TileGrid(
     icons_large_bmp,
@@ -132,49 +177,25 @@ curcond = label.Label(terminalio.FONT, text="Thunderstorm", color=0x000000)
 curcond.anchor_point = (0.5,1)
 curcond.anchored_position = (47,115)
 
-curtemp = label.Label(terminalio.FONT, text="+100F", color=0x000000)
-curtemp.anchor_point = (0, 0)
-curtemp.anchored_position = (150, 21)
-
-daylo = label.Label(terminalio.FONT, text="+100F", color=0x000000)
-daylo.anchor_point = (0, 0)
-daylo.anchored_position = (100, 50)
-
-dayhi = label.Label(terminalio.FONT, text="+100F", color=0x000000)
-dayhi.anchor_point = (0, 0)
-dayhi.anchored_position = (130, 50)
-
-dayprecip = label.Label(terminalio.FONT, text="...", color=0x000000)
-dayprecip.anchor_point = (0, 0)
-dayprecip.anchored_position = (165, 50)
-
-curhumid = label.Label(terminalio.FONT, text="100%", color=0x000000)
-curhumid.anchor_point = (0, 0)
-curhumid.anchored_position = (118, 70)
-
-curwind = label.Label(terminalio.FONT, text="99m/s", color=0x000000)
-curwind.anchor_point = (0, 0)
-curwind.anchored_position = (150, 70)
-
-curprs = label.Label(terminalio.FONT, text="99m/s", color=0x000000)
-curprs.anchor_point = (0, 0)
-curprs.anchored_position = (105, 94)
-
-curuv = label.Label(terminalio.FONT, text="100%", color=0x000000)
-curuv.anchor_point = (0, 0)
-curuv.anchored_position = (160, 94)
-
-curcloud = label.Label(terminalio.FONT, text="100", color=0x000000)
-curcloud.anchor_point = (0, 0)
-curcloud.anchored_position = (165, 50)
-
-sunrise = label.Label(terminalio.FONT, text="12:12 PM", color=0x000000)
+sunrise = label.Label(terminalio.FONT, text="12:12 AM", color=0x000000)
 sunrise.anchor_point = (0, 0.5)
 sunrise.anchored_position = (45, 120)
 
 sunset = label.Label(terminalio.FONT, text="12:12 PM", color=0x000000)
 sunset.anchor_point = (0, 0.5)
 sunset.anchored_position = (130, 120)
+	
+dayhi = label.Label(terminalio.FONT, text="+100F", color=0x000000)
+dayhi.anchor_point = (0, 0)
+dayhi.anchored_position = (130, 50)
+
+daylo = label.Label(terminalio.FONT, text="+100F", color=0x000000)
+daylo.anchor_point = (0, 0)
+daylo.anchored_position = (100, 50)
+
+dayprecip = label.Label(terminalio.FONT, text="...", color=0x000000)
+dayprecip.anchor_point = (0, 0)
+dayprecip.anchored_position = (165, 50)
 
 today_banner = displayio.Group()
 today_banner.append(curdt)
@@ -209,9 +230,9 @@ magtag.splash.append(today_banner)
 for future_banner in future_banners:
     magtag.splash.append(future_banner)
 
-# ===========
-#  M A I N
-# ===========
+# =========================================================================
+#  Main section of the program to fill in the data and update the display
+# =========================================================================
 
 #***** Pull in Weather and AQI Data
 print("Fetching forecast from OWM")
@@ -219,18 +240,18 @@ final_url = weather_url + "appid=" + key + "&lat=" + lat + "&lon=" + lon
 weather_data = magtag.network.fetch(final_url).json()
 final_url = aqi_url + "appid=" + key + "&lat=" + lat + "&lon=" + lon
 aqi_data = magtag.network.fetch(final_url).json()
-
-#***** Setup buckets of data
 print()
-print("Setting up data buckets")
+
+#***** Moving data to the right bucket to process
+print("Moving data to buckets")
 tzoffset = weather_data["timezone_offset"]
 currentd = weather_data["current"] 
 utc_time = currentd["dt"]
 todayd = weather_data["daily"][0]
 forecast = weather_data["daily"][1:6]
 
-
 print("Updating data labels")
+#***** Setting up the local time and date
 date = time.localtime(utc_time + tzoffset)
 sunr = time.localtime(currentd["sunrise"] + tzoffset)
 suns = time.localtime(currentd["sunset"] + tzoffset)
@@ -245,6 +266,8 @@ curtim.text = "As of: {:d}:{:02d}".format(
         date.tm_hour,
         date.tm_min,
     )
+
+#***** Fill in the data for current conditions
 curtemp.text = "{:3.0f}".format(currentd["temp"])
 #curfeels.text = currentd["feels_like"]
 curprs.text = "{:2.2f}".format(currentd["pressure"] * 0.02952998751)
@@ -259,11 +282,11 @@ curicon[0] = ICON_MAP.index(currentd["weather"][0]["icon"][:2])
 curcond.text = currentd["weather"][0]["main"]
 sunrise.text = "{:2d}:{:02d} AM".format(sunr.tm_hour, sunr.tm_min)
 sunset.text = "{:2d}:{:02d} PM".format(suns.tm_hour - 12, suns.tm_min)
-
 dayhi.text = "{:3.0f}".format(todayd["temp"]["max"])
 daylo.text = "{:3.0f}".format(todayd["temp"]["min"])
 dayprecip.text = "{:2.0f}%".format(todayd["pop"] * 100)
 
+#***** Fill in the table for future forecast
 for day, forecast in enumerate(forecast[0:5]):
     update_banner(future_banners[day], forecast)
 
@@ -273,6 +296,21 @@ magtag.display.refresh()
 time.sleep(magtag.display.time_to_refresh + 1)
 
 print("Sleeping...")
-go_to_sleep(utc_time + tzoffset)
 #  entire code will run again after deep sleep cycle
 #  similar to hitting the reset button
+hour, minutes, seconds = time.localtime(current_time)[3:6]
+# if its after 10pm, sleep till 5 am.  if not, sleep for 15 mins
+if hour >21:
+        seconds_to_sleep = (((24-hour)*3600)-(minutes*60) + (3600*5))
+    else:
+        seconds_to_sleep = 60*15
+    print(
+        "Sleeping for {} hours, {} minutes".format(
+            seconds_to_sleep // 3600, (seconds_to_sleep // 60) % 60
+        )
+    )
+magtag.exit_and_deep_sleep(seconds_to_sleep)
+
+# =========================================================================
+# End of program
+# =========================================================================
